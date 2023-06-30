864a72b642ad958a8cef1d171543ceea6519ef65
HopKaa <127760062+HopKaa@users.noreply.github.com>
2023-06-30 20:24:07 +0700

    implemented the appearance of numbers on the playing field

--- Assets/Scripts/GameSessionManager.cs
+++ Assets/Scripts/GameSessionManager.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 
 public class GameSessionManager : MonoBehaviour
 {
-    public float _sessionTime = 60f;
-    public float _remainingTime;
-    public Slider _progressBar;
-    public Text _timerText;
-    public Text _resultText;
-    public List<int> _numbersToClick = new List<int> { 1 };
+    [SerializeField] private Transform _panelTransform;
+    [SerializeField] private Slider _progressBar;
+    [SerializeField] private Text _timerText;
+    [SerializeField] private Text _resultText;
+    [SerializeField ] private GameObject _buttonPrefab;
+    [SerializeField] private Slider _slider;
+
+    private int _minNumber = 1;
+    private int _maxNumber = 9;
+    private float _remainingTime;
     private int _currentNumberIndex;
     private bool _sessionInProgress;
 
+    public float _sessionTime = 60f;
+    public List<int> _numbersToClick = new List<int> { 1 };
     public void StartSession()
     {
         _remainingTime = _sessionTime;
@@ -22,7 +28,7 @@ public class GameSessionManager : MonoBehaviour
         _currentNumberIndex = 0;
         _sessionInProgress = true;
 
-        
+        SpawnButtons();
         StartCoroutine(Countdown());
     }
 
@@ -78,5 +84,56 @@ public class GameSessionManager : MonoBehaviour
         {
             EndSession(false);
         }
+    }
+
+    public void SpawnButtons()
+    {
+        int numberOfButtons = (int)_slider.value;
+
+        if (numberOfButtons < _minNumber)
+        {
+            numberOfButtons = _minNumber;
+        }
+        else if (numberOfButtons > _maxNumber)
+        {
+            numberOfButtons = _maxNumber;
+        }
+
+        int minValue = 1;
+        int maxValue = numberOfButtons;
+
+        for (int i = 0; i < numberOfButtons; i++)
+        {
+            GameObject buttonObject = Instantiate(_buttonPrefab);
+            buttonObject.transform.SetParent(_panelTransform);
+
+            RectTransform panelRect = _panelTransform.GetComponent<RectTransform>();
+            Vector2 randomPosition = GetRandomPositionInPanel(panelRect);
+
+            buttonObject.transform.localPosition = randomPosition;
+
+            Text buttonText = buttonObject.GetComponentInChildren<Text>();
+            buttonText.text = (minValue + i).ToString();
+
+            buttonObject.GetComponent<Button>().onClick.AddListener(() => DestroyButton(buttonObject));
+        }
+    }
+
+    private Vector2 GetRandomPositionInPanel(RectTransform panelRect)
+    {
+        float minX = panelRect.rect.xMin;
+        float maxX = panelRect.rect.xMax;
+        float minY = panelRect.rect.yMin;
+        float maxY = panelRect.rect.yMax;
+
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        return new Vector2(randomX, randomY);
+    }
+
+    private void DestroyButton(GameObject buttonObject)
+    {
+        Destroy(buttonObject);
     }
 }
--- Assets/Scripts/GameSettingsManager.cs
+++ Assets/Scripts/GameSettingsManager.cs
@@ -4,20 +4,8 @@ using UnityEngine.UI;
 
 public class GameSettingsManager : MonoBehaviour
 {
-    public Slider _numbersSlider;
     public Slider _timeSlider;
     public GameSessionManager _gameSessionManager;
-
-    public void OnNumbersSliderChange()
-    {
-        int numbersCount = (int)_numbersSlider.value;
-        _gameSessionManager._numbersToClick.Clear();
-
-        for (int i = 1; i <= numbersCount; i++)
-        {
-            _gameSessionManager._numbersToClick.Add(i);
-        }
-    }
 
     public void OnTimeSliderChange()
     {
