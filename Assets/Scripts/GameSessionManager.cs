f6701f91437cf18d0dcfed0173b0631ef9f6b1e7
HopKaa <127760062+HopKaa@users.noreply.github.com>
2023-07-07 18:24:56 +0700

    Changed creation and deletion of the game

--- Assets/Scripts/GameSessionManager.cs
+++ Assets/Scripts/GameSessionManager.cs
@@ -5,34 +5,28 @@ using UnityEngine.UI;
 
 public class GameSessionManager : MonoBehaviour
 {
+    private const float _defaultSessionTime = 60f;
+    private const string _resultFail = "Fail";
+    private const string _resultSuccess = "Success";
+    private const int FieldWidth = 850;
+    private const int FieldHeight = 300;
+
     [SerializeField] private Transform _panelTransform;
     [SerializeField] private Slider _progressBar;
     [SerializeField] private Text _timerText;
     [SerializeField] private Text _resultText;
     [SerializeField ] private GameObject _buttonPrefab;
     [SerializeField] private Slider _slider;
+    [SerializeField] public GameObject _field;
+    [SerializeField] private Slider _timeSlider;
 
-    private int _minNumber = 1;
-    private int _maxNumber = 9;
     private float _remainingTime;
-    private int _currentNumberIndex;
+    private int _currentNumberMax;
     private bool _sessionInProgress;
+    private float _sessionTime = _defaultSessionTime;
 
-    public float _sessionTime = 60f;
-
-    private List<int> _numbersToClick = new List<int> ();
-    private List<int> _clickedNumbers = new List<int> ();
-
-    private void StartSession()
-    {
-        _remainingTime = _sessionTime;
-        _progressBar.value = 1f;
-        _resultText.text = "";
-        _currentNumberIndex = 0;
-        _sessionInProgress = true;
-
-        StartCoroutine(Countdown());
-    }
+    private int _currentNumber;
+    private readonly List<Number> _numbers = new();
 
     private void EndSession(bool success)
     {
@@ -40,30 +34,43 @@ public class GameSessionManager : MonoBehaviour
 
         if (success)
         {
-            _resultText.text = "Success";
+            _resultText.text = _resultSuccess;
         }
         else
         {
-            _resultText.text = "Fail";
+            _resultText.text = _resultFail;
         }
         StopAllCoroutines();
         _remainingTime = 0f;
         _progressBar.value = 0f;
     }
-    private void RestartSession()
+    private void StartSession()
     {
-        _resultText.text = "";
+        InitValues();
         ClearPanel();
-        SpawnButtons();
-        _clickedNumbers.Clear();
-        StartSession();
+        SpawnNumbers();
+
+        StartCoroutine(Countdown());
+    }
+
+    private void InitValues()
+    {
+        _currentNumber = 0;
+        _remainingTime = _sessionTime;
+        _progressBar.value = 1f;
+        _resultText.text = "";
+        _sessionInProgress = true;
     }
     private void ClearPanel()
     {
-        foreach (Transform button in _panelTransform)
+        foreach (Number number in _numbers)
         {
-            Destroy(button.gameObject);
+            if (number != null)
+            {
+                Destroy(number.gameObject);
+            }
         }
+        _numbers.Clear();
     }
     IEnumerator Countdown()
     {
@@ -75,106 +82,46 @@ public class GameSessionManager : MonoBehaviour
 
             yield return null;
         }
-
         EndSession(false);
     }
-
-    private void CheckClick(int clickedNumber)
+    public void OnTimeSliderChange()
     {
-        if (!_sessionInProgress)
-            return;
-
-        if (clickedNumber != _numbersToClick[_currentNumberIndex])
-        {
-            EndSession(false);
-            return;
-        }
-
-        _currentNumberIndex++;
-
-        _clickedNumbers.Add(clickedNumber);
-
-        if (_currentNumberIndex >= _numbersToClick.Count)
-        {
-            EndSession(true);
-        }
+        _sessionTime = _timeSlider.value;
     }
 
-    private void SpawnButtons()
+    private void SpawnNumbers()
     {
-        int numberOfButtons = (int)_slider.value;
+        _currentNumberMax = (int)_slider.value;
 
-        if (numberOfButtons < _minNumber)
-        {
-            numberOfButtons = _minNumber;
-        }
-        else if (numberOfButtons > _maxNumber)
-        {
-            numberOfButtons = _maxNumber;
-        }
-
-        _numbersToClick.Clear();
-
-        for (int i = 1; i <= numberOfButtons; i++)
-        {
-            _numbersToClick.Add(i);
-        }
-
-        _numbersToClick = new List<int>();
-
-        int maxValue = (int)_slider.value;
-
-        if (maxValue < _minNumber)
-        {
-            maxValue = _minNumber;
-        }
-        else if (maxValue > _maxNumber)
-        {
-            maxValue = _maxNumber;
-        }
-
-        for (int i = 1; i <= maxValue; i++)
-        {
-            _numbersToClick.Add(i);
-        }
-
-        for (int i = 0; i < numberOfButtons; i++)
+        for (int i = 0; i < _currentNumberMax; i++)
         {
             GameObject buttonObject = Instantiate(_buttonPrefab);
             buttonObject.transform.SetParent(_panelTransform);
-            buttonObject.GetComponent<Number>().Initialized(this);
+            Rect _fieldSize = new Rect(0, 0, FieldWidth, FieldHeight);
 
-            RectTransform panelRect = _panelTransform.GetComponent<RectTransform>();
-            Vector2 randomPosition = GetRandomPositionInPanel(panelRect);
+            buttonObject.GetComponent<Number>().Initialized(this, i, _fieldSize);
 
-            buttonObject.transform.localPosition = randomPosition;
-
-            Text buttonText = buttonObject.GetComponentInChildren<Text>();
-            buttonText.text = _numbersToClick[i].ToString();
+            _numbers.Add(buttonObject.GetComponent<Number>());
         }
     }
 
-    private Vector2 GetRandomPositionInPanel(RectTransform panelRect)
-    {
-        float minX = panelRect.rect.xMin;
-        float maxX = panelRect.rect.xMax;
-        float minY = panelRect.rect.yMin;
-        float maxY = panelRect.rect.yMax;
-
-        float randomX = Random.Range(minX, maxX);
-        float randomY = Random.Range(minY, maxY);
-
-        return new Vector2(randomX, randomY);
-    }
-
-    public void HandleButtonClick(GameObject buttonObject)
+    public void HandleButtonClick(int value)
     {
         if (!_sessionInProgress)
+        {
             return;
+        }    
 
-        int clickedNumber = int.Parse(buttonObject.GetComponentInChildren<Text>().text);
-        CheckClick(clickedNumber);
-      
-        Destroy(buttonObject);
+        if(_currentNumber != value)
+        {
+            EndSession(false);
+        }
+
+        _currentNumber++;
+
+        if (_currentNumber >= _currentNumberMax)
+        {
+            EndSession(true);
+        }
     }
 }
--- Assets/Scripts/Number.cs
+++ Assets/Scripts/Number.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Number : MonoBehaviour
 {
+    private int _value;
+    private GameSessionManager _gameSessionManager;
+
     public void OnClick()
     {
-        if (_gameSessionManager != null)
-        {
-            _gameSessionManager.HandleButtonClick(gameObject);
-        }
+        _gameSessionManager.HandleButtonClick(_value);
+        Destroy(gameObject);
     }
-    public void Initialized(GameSessionManager gameSessionManager)
+
+    public void Initialized(GameSessionManager gameSessionManager, int value, Rect fieldSize)
     {
         _gameSessionManager = gameSessionManager;
-
+        SetRandomPosition(fieldSize);
+        _value = value;
+        Text buttonText = GetComponentInChildren<Text>();
+        buttonText.text = (value + 1).ToString();
     }
-    private GameSessionManager _gameSessionManager;
+
+    private void SetRandomPosition(Rect fieldSize)
+    {
+        float minX = _gameSessionManager._field.transform.position.x - fieldSize.width;
+        float maxX = _gameSessionManager._field.transform.position.x + fieldSize.width;
+        float minY = _gameSessionManager._field.transform.position.y - fieldSize.height;
+        float maxY = _gameSessionManager._field.transform.position.y + fieldSize.height;
+
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        transform.position = new Vector3(randomX, randomY, 0f);
+    }
 }
